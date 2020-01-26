0753496c687fac33d09eeab5985ef6ddcf1a060e
Davyd McColl <davydm@gmail.com>
2020-01-26 21:22:41 +0200

    :zap: optimise throughput a bit

--- src/bitsplat/Pipes/StreamSource.cs
+++ src/bitsplat/Pipes/StreamSource.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 
 namespace bitsplat.Pipes
@@ -11,11 +13,17 @@ namespace bitsplat.Pipes
         private int _readSize;
         private int _lastRead = 0;
 
+        private static int _lastReadSize;
+
         public StreamSource(Stream source)
             : this(source, true)
         {
             _readSize = ReadChunkIncrement;
             _lastRead = ReadChunkIncrement;
+            if (_lastReadSize > _readSize)
+            {
+                _readSize = _lastReadSize;
+            }
         }
 
         public StreamSource(
@@ -24,37 +32,28 @@ namespace bitsplat.Pipes
         {
             _source = source;
             _disposeAtEnd = disposeAtEnd;
+            _stopwatch = new Stopwatch();
         }
-        
-        public static int MaxBuffer = 5 * 1024 * 1024;
-        public static int ReadChunkIncrement = 32768;
+
+        public static int MaxBuffer = 32 * 1024 * 1024; // 32M
+        public static int ReadChunkIncrement = 131072; // 128K
+        private readonly Stopwatch _stopwatch;
 
         public bool Pump()
         {
             using var pooledBuffer = BufferPool.Borrow(MaxBuffer);
             var buffer = pooledBuffer.Data;
-            
-            // TODO: instead of just increasing buffer sizes
-            //       to a max, since sinks flush at write
-            //       the read buffer size should work towards about
-            //       what can be written in a second to keep the
-            //       app interactive to, eg, SIGINT
-            // -> use case: copying over wifi, smb, max rate is 4mb/s
-            //    but buffer size is 16, so the app is waiting for 4 seconds
-            //    per flush, which translates into a 4 second delay when
-            //    attempting to exit as streams are flushing
-            var toRead = _lastRead < _readSize
-                         ? _lastRead
-                         : _lastRead + ReadChunkIncrement;
-            if (toRead > MaxBuffer)
-            {
-                toRead = MaxBuffer;
-            }
 
+            var toRead = CalculateReadBufferSize();
+
+            _readSize = toRead;
             _lastRead = _source?.Read(buffer, 0, toRead) ?? 0;
+
             if (_lastRead > 0)
             {
-                _sinks.ForEach(sink => sink.Write(buffer, _lastRead));
+                Time(() =>
+                    _sinks.ForEach(sink => sink.Write(buffer, _lastRead))
+                );
             }
             else
             {
@@ -68,6 +67,34 @@ namespace bitsplat.Pipes
             }
 
             return _lastRead > 0;
+        }
+
+        private int CalculateReadBufferSize()
+        {
+            var toRead = (_lastRead < _readSize || _stopwatch.ElapsedMilliseconds >= 1000)
+                             ? _lastRead
+                             : _lastRead + ReadChunkIncrement;
+            if (_stopwatch.ElapsedMilliseconds >= 1500 &&
+                toRead > ReadChunkIncrement)
+            {
+                // syncs are taking > 1.5 ms -- back off a little to keep the app interactive
+                toRead -= ReadChunkIncrement;
+            }
+
+            if (toRead > MaxBuffer)
+            {
+                toRead = MaxBuffer;
+            }
+
+            return toRead;
+        }
+
+        private void Time(Action action)
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+            action();
+            _stopwatch.Stop();
         }
 
         public ISink Pipe(ISink sink)
@@ -99,6 +126,7 @@ namespace bitsplat.Pipes
 
         public void Dispose()
         {
+            _lastReadSize = _readSize; // let the next instance start at the same read size
             var sinks = _sinks.ToArray();
             _sinks.Clear();
             var source = _source;
