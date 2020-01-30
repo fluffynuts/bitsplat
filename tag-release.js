2fdb462a2b2542813b68a98c61882de6fd38d253
Davyd McColl <davydm@gmail.com>
2020-01-30 14:15:44 +0200

    :bookmark: release v1.1

---
+++ tag-release.js
@@ -0,0 +1,21 @@
+const 
+    promisify = require("util").promisify,
+    exec = promisify(require("child_process").exec);
+
+(async function() {
+    const 
+        execResult = await exec("git tag | head -n 1"),
+        latestTag = execResult.stdout.trim(),
+        latestTagVersion = latestTag.replace(/[^0-9.]*/g, ""),
+        asFloat = parseFloat(latestTagVersion),
+        next = asFloat + 0.1,
+        cmd1 = `git add -A :/`,
+        cmd2 = `git commit -m ":bookmark: release v${next}"`,
+        cmd3 = `git tag v${next}`;
+    console.log(`commit release v${next}`);
+    await exec(cmd1);
+    console.log(`tagging at v${next}`);
+    await exec(cmd2);
+    console.log(`pushing...`);
+    await exec(cmd3);
+})();
